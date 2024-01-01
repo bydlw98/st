2c7eea0dc3d0985a4290ef8e8789606de0f3e361
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-01-01 18:48:14 +0800

    feat: configure borderpx and color settings

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 8;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -95,33 +95,29 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+    /* 8 normal colors */
+    [0] = "#282c34",  /* black   */
+    [1] = "#e06c75",  /* red     */
+    [2] = "#98c379",  /* green   */
+    [3] = "#e5c07b",  /* yellow  */
+    [4] = "#61afef",  /* blue    */
+    [5] = "#c678dd",  /* magenta */
+    [6] = "#56b6c2",  /* cyan    */
+    [7] = "#dcdfe4",  /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+    /* 8 bright colors */
+    [8] = "#282c34",  /* black   */
+    [9] = "#e06c75",  /* red     */
+    [10] = "#98c379", /* green   */
+    [11] = "#e5c07b", /* yellow  */
+    [12] = "#61afef", /* blue    */
+    [13] = "#c678dd", /* magenta */
+    [14] = "#56b6c2", /* cyan    */
+    [15] = "#dcdfe4", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+    /* special colors */
+    [256] = "#282c34", /* background */
+    [257] = "#dcdfe4", /* foreground */
 };
 
 
@@ -129,10 +125,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
