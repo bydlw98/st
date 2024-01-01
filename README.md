ebb6da09a2d3315458ff6339e5426a73f8d9c64d
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-01-01 20:14:43 +0800

    feat: apply font2 patch

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 My build of [st](https://st.suckless.org/) the simple terminal implementation for X by suckless.
 
+## Patches
+
+* [font2](https://st.suckless.org/patches/font2/)
+
 ## Installation
 ```
 git clone https://github.com/bydlw98/st.git
--- config.h
+++ config.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Symbols Nerd Font Mono:pixelsize=14:antialias=true:autohint=true"
+};
+
 static int borderpx = 8;
 
 /*
